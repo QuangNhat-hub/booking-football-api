f9d3f691c338630c106d764e5b37cbdcb94474bb
nguyenphuba2k5-byte <nguyenphuba2k5@gmail.com>
2026-06-04 17:08:34 +0700

    Add CancelBookingRequestDTO class for booking cancellation

---
+++ src/main/java/com/booking/football_api/dto/CancelBookingRequestDTO.java
@@ -0,0 +1,11 @@
+package com.booking.football_api.dto;
+
+public class CancelBookingRequestDTO {
+	private String reason;
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+}
